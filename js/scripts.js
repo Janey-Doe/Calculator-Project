a6ab8c5727002bc0928bbc54b115b697ab3721b1
Mercy Rigua <mercyrigua@gmail.com>
2018-04-24 16:44:12 +0300

    calculator bmi

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+      var weight= parseFloat(prompt("Enter your weight:"));
+      var height = parseFloat(prompt("Enter your height:"));
+      var result = (weight/height)/height;
+      alert(result);
