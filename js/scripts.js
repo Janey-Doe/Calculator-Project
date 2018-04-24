c181d4aa6531fb703979d284513fa26ff9d7664f
Mercy Rigua <mercyrigua@gmail.com>
2018-04-24 17:02:29 +0300

    Add addition function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 
-      var weight= parseFloat(prompt("Enter your weight:"));
-      var height = parseFloat(prompt("Enter your height:"));
-      var result = (weight/height)/height;
-      alert(result);
+//add function
+      var add = function(number1, number2) {
+              return number1 + number2;
+            };
+            var number1 = parseInt(prompt("Enter a number:"));
+            var number2 = parseInt(prompt("Enter another number:"));
+            var result = add(number1, number2);
+            alert(result);
